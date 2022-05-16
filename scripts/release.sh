86d6907b814154a55db419570bd46089a21fa4ec
Huy Le <lehuydit@gmail.com>
2022-05-16 19:59:59 +0700

    chore: test build fastlane beta

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,2 +1,4 @@
-sudo keytool -genkey -v -keystore my-upload-key.keystore -alias michaelhuy -keyalg RSA -keysize 2048 -validity 10000
+# sudo keytool -genkey -v -keystore my-upload-key.keystore -alias michaelhuy -keyalg RSA -keysize 2048 -validity 10000
 
+base64 --input my-upload-key.keystore --output keystore.b64
+# base64 -d --input [File].b64 --output [File]
