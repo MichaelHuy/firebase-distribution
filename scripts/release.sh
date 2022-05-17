2f8fcc450487ea319859c24922711998e0955024
Huy Le <lehuydit@gmail.com>
2022-05-17 14:06:01 +0700

    chore: firebase distribution

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,6 @@
 # sudo keytool -genkey -v -keystore my-upload-key.keystore -alias michaelhuy -keyalg RSA -keysize 2048 -validity 10000
 
-base64 --input my-upload-key.keystore --output keystore.b64
+# base64 --input my-upload-key  .keystore --output keystore.b64
 # base64 -d --input [File].b64 --output [File]
+
+base64 --input firebase_credentials.json --output firebase_credentials.json.b64
