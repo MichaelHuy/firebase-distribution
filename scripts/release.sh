a0fb654e856fe8f64ad58df5c9c453ec15268a35
Huy Le <lehuydit@gmail.com>
2022-05-23 09:44:39 +0700

    chore: test submit internal

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,4 +3,6 @@
 # base64 --input my-upload-key  .keystore --output keystore.b64
 # base64 -d --input [File].b64 --output [File]
 
-base64 --input fir-distribution-6fa15-5dd8355cc0c0.json --output firebase_credentials.json.b64
+# base64 --input fir-distribution-6fa15-5dd8355cc0c0.json --output firebase_credentials.json.b64
+
+base64 --input play-store-credentials.json --output play-store-credentials.json.b64
